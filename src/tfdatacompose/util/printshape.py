f9f71a22e54cecb4f3b68f4e59f0ebc9089eb6eb
François LaBerge <fgrclaberge@gmail.com>
2023-11-21 08:54:12 -0500

    use documentation for printing operations

--- src/tfdatacompose/util/printshape.py
+++ src/tfdatacompose/util/printshape.py
@@ -5,6 +5,15 @@ from tfdatacompose.map.map import Map
 
 
 class PrintShape(Map):
+    """
+    Print tensor shape operation.
+
+    Prints the dataset element's shape, then forwards the elements unchanged.
+    This operation is mainly used for debugging.
+
+    :param name: The name of the operation to display when printing, if None a number is displayed instead.
+    """
+
     number = 1
 
     def __init__(self, name: str | None = None):
